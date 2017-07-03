2aea79760679b183eb5a4a6b7e14df6c005b5d00
Thatchapon Unprasert <poomst@hotmail.com>
2017-07-03 10:15:42 +0700

    Workaround for iOS <= 8.x SDKs.

--- respring_simulator/respring_simulator.mm
+++ respring_simulator/respring_simulator.mm
@@ -1,11 +1,20 @@
 #import "../simjectCore.h"
 
-int main(int argc, const char * argv[]) {
-	printf("respring_simulator (C) 2016 Karen Tsai (angelXwind)\n");
-	printf("Injecting appropriate dynamic libraries from /opt/simject...\n");
-	NSString *envVars;
-	system([[NSString stringWithFormat:@"xcrun simctl spawn booted launchctl debug system/com.apple.SpringBoard%@", (envVars = simjectGenerateDylibList(nil, nil)) ? [NSString stringWithFormat:@" --environment DYLD_INSERT_LIBRARIES=%@", envVars] : nil] UTF8String]);
-	printf("Respringing...\n");
-	system("xcrun simctl spawn booted launchctl stop com.apple.SpringBoard");
-	return 0;
+int main(int argc, const char *argv[]) {
+    printf("respring_simulator (C) 2016 Karen Tsai (angelXwind)\n");
+    printf("Injecting appropriate dynamic libraries from /opt/simject...\n");
+    NSString *envVars;
+    int result = system("xcrun simctl spawn booted launchctl debug > /dev/null");
+    if (!result) {
+        system([[NSString stringWithFormat:@"xcrun simctl spawn booted launchctl debug system/com.apple.SpringBoard%@", (envVars = simjectGenerateDylibList(nil, nil)) ? [NSString stringWithFormat:@" --environment DYLD_INSERT_LIBRARIES=%@", envVars] : nil] UTF8String]);
+    } else {
+        printf("Using another method...\n");
+        system([[NSString stringWithFormat:@"xcrun simctl spawn booted launchctl setenv%@", (envVars = simjectGenerateDylibList(nil, nil)) ? [NSString stringWithFormat:@" DYLD_INSERT_LIBRARIES \"%@\"", envVars] : nil] UTF8String]);
+    }
+    printf("Respringing...\n");
+    system("xcrun simctl spawn booted launchctl stop com.apple.SpringBoard");
+    if (!result) {
+        system("xcrun simctl spawn booted launchctl unsetenv DYLD_INSERT_LIBRARIES");
+    }
+    return 0;
 }
