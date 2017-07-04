073746c811c30a57bd201b0634c2f215f60fc66b
Thatchapon Unprasert <poomst@hotmail.com>
2017-07-04 17:35:46 +0700

    This legacy method is very similar to the actual CydiaSubstrate.

--- respring_simulator/respring_simulator.mm
+++ respring_simulator/respring_simulator.mm
@@ -1,20 +1,8 @@
-#import "../simjectCore.h"
-
 int main(int argc, const char *argv[]) {
     printf("respring_simulator (C) 2016 Karen Tsai (angelXwind)\n");
     printf("Injecting appropriate dynamic libraries from /opt/simject...\n");
-    NSString *envVars;
-    int result = system("[[ `xcrun simctl spawn booted launchctl version | grep -oE \"\\d\\.\\d\\.\\d:\" | grep -oE \"^\\d\"` -le 2 ]]");
-    if (result) {
-        system([[NSString stringWithFormat:@"xcrun simctl spawn booted launchctl debug system/com.apple.SpringBoard%@", (envVars = simjectGenerateDylibList(nil, nil)) ? [NSString stringWithFormat:@" --environment DYLD_INSERT_LIBRARIES=%@", envVars] : nil] UTF8String]);
-    } else {
-        printf("Using another method...\n");
-        system([[NSString stringWithFormat:@"xcrun simctl spawn booted launchctl setenv%@", (envVars = simjectGenerateDylibList(nil, nil)) ? [NSString stringWithFormat:@" DYLD_INSERT_LIBRARIES \"%@\"", envVars] : nil] UTF8String]);
-    }
+    system("xcrun simctl spawn booted launchctl setenv DYLD_INSERT_LIBRARIES /opt/simject/simject.dylib");
     printf("Respringing...\n");
     system("xcrun simctl spawn booted launchctl stop com.apple.SpringBoard");
-    if (!result) {
-        system("xcrun simctl spawn booted launchctl unsetenv DYLD_INSERT_LIBRARIES");
-    }
     return 0;
 }
