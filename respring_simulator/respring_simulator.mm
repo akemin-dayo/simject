ec191f023c452c0e4bc8106f32bda1a5baefc385
Karen Tsai <angelXwind@angelxwind.net>
2017-08-04 01:22:43 -0400

    Reworded most of the text found in simject, made the syntax (always using brackets after if statements) and indentation style (tabs) consistent, prettified the respring_simulator help dialog.

--- respring_simulator/respring_simulator.mm
+++ respring_simulator/respring_simulator.mm
@@ -11,196 +11,210 @@ using namespace std;
 
 BOOL iOS7 = NO;
 
+void globalHeader() {
+	printf("respring_simulator (C) 2016-2017 Karen／明美 (angelXwind)\n");
+}
+
 void printUsage() {
-    printf("\nUsage:\n");
-    printf("\nRespring the latest booted device:\n\n");
-    printf("\trespring_simulator\n");
-    printf("\nRespring the booted device with matching type and version:\n\n");
-    printf("\trespring_simulator -d \"iPhone 5\" -v 8.1\n");
-    printf("\t(Will respring iPhone 5 simulator running iOS 8.1)\n");
-    printf("\nRespring the booted device with matching UDID:\n\n");
-    printf("\trespring_simulator -i 5AA1C45D-DB69-4C52-A75B-E9BE9C7E7770\n");
-    printf("\t(Will respring simulator with UDID 5AA1C45D-DB69-4C52-A75B-E9BE9C7E7770)\n");
-    printf("\nRespring any booted simulator:\n\n");
-    printf("\trespring_simulator all\n");
-    printf("\nFor iOS 7 runtime (Xcode <= 6.2 w/o multi-simulator), -l flag is needed:\n\n");
-    printf("\trespring_simulator -l\n");
-    printf("\n");
+	globalHeader();
+	printf("\nUsage: respring_simulator [options]\n");
+	printf("Example: respring_simulator -d \"iPhone 5\" -v 8.1\n");
+	printf("\nAvailable options:\n");
+	printf("\t-h    Shows this usage dialog\n");
+	printf("\t-d    Specifies a device type\n");
+	printf("\t-v    Specifies an iOS version\n");
+	printf("\t-i    Specifies a UUID corresponding to a specific iOS Simulator\n");
+	printf("\t-l    Enables iOS 7 compatibility mode (simject will not work with the iOS 7 runtime without this)\n");
+	printf("\tall   Resprings all booted iOS Simulators\n");
+	printf("\nExample usages:\n");
+	printf("\tRespring a booted device matching the specified device type and iOS version\n");
+	printf("\t\trespring_simulator -d \"iPhone 5\" -v 8.1\n");
+	printf("\tRespring a booted device with the specified UDID\n");
+	printf("\t\trespring_simulator -i 5AA1C45D-DB69-4C52-A75B-E9BE9C7E7770\n");
+	printf("\tRespring all booted iOS Simulators\n");
+	printf("\t\trespring_simulator all\n");
+	printf("\tRespring an iOS Simulator using the iOS 7 runtime (Xcode <= 6.2)\n");
+	printf("\t\trespring_simulator -l\n");
 }
 
 string exec(const char *cmd) {
-    array<char, 128> buffer;
-    string result;
-    shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-    if (pipe) {
-        while (!feof(pipe.get())) {
-            if (fgets(buffer.data(), 128, pipe.get()) != NULL)
-                result += buffer.data();
-        }
-    }
-    return result;
+	array<char, 128> buffer;
+	string result;
+	shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+	if (pipe) {
+		while (!feof(pipe.get())) {
+			if (fgets(buffer.data(), 128, pipe.get()) != NULL)
+				result += buffer.data();
+		}
+	}
+	return result;
 }
 
 void injectHeader() {
-    printf("respring_simulator (C) 2016-2017 Karen／明美 (angelXwind)\n");
-    printf("Injecting appropriate dynamic libraries from /opt/simject...\n");
+	globalHeader();
+	printf("Injecting appropriate dynamic libraries from /opt/simject...\n");
 }
 
 void inject(const char *udid, const char *device, BOOL _exit) {
-    if (device) {
-        printf("Respringing %s (%s) ...\n", udid, device);
-    } else {
-        printf("Respringing %s ...\n", !strcmp(udid, "booted") ? "a booted device" : udid);
-    }
-    pid_t pid = fork();
-    if (pid == 0) {
-    	if (iOS7) {
-    		if (!strcmp(udid, "booted")) {
-    			string sudid = exec("xcrun simctl list devices | grep -E Booted | grep -oE \"\\([A-Z0-9\\-]+\\)\" | sed \"s/[()]//g\"");
-    			if (!sudid.empty())
-    				sudid.erase(sudid.length() - 1);
-    			udid = strdup(sudid.c_str());
-    		}
-    		system([[NSString stringWithFormat:@"plutil -replace bootstrap.child.DYLD_INSERT_LIBRARIES -string /opt/simject/simject.dylib %@/Library/Developer/CoreSimulator/Devices/%@/data/var/run/launchd_bootstrap.plist -s", NSHomeDirectory(), @(udid)] UTF8String]);
-    		system("killall launchd_sim");
-    	} else {
-        	system([[NSString stringWithFormat:@"xcrun simctl spawn %s launchctl setenv DYLD_INSERT_LIBRARIES /opt/simject/simject.dylib", udid] UTF8String]);
-        	system([[NSString stringWithFormat:@"xcrun simctl spawn %s launchctl setenv __XPC_DYLD_INSERT_LIBRARIES /opt/simject/simject.dylib", udid] UTF8String]);
-        	system([[NSString stringWithFormat:@"xcrun simctl spawn %s launchctl stop com.apple.backboardd", udid] UTF8String]);
-        }
-        exit(EXIT_SUCCESS);
-    } else {
-        if (_exit)
-            exit(EXIT_SUCCESS);
-    }
+	if (device) {
+		printf("Respringing %s (%s) ...\n", udid, device);
+	} else {
+		printf("Respringing %s ...\n", !strcmp(udid, "booted") ? "a booted device" : udid);
+	}
+	pid_t pid = fork();
+	if (pid == 0) {
+		if (iOS7) {
+			if (!strcmp(udid, "booted")) {
+				string sudid = exec("xcrun simctl list devices | grep -E Booted | grep -oE \"\\([A-Z0-9\\-]+\\)\" | sed \"s/[()]//g\"");
+				if (!sudid.empty())
+					sudid.erase(sudid.length() - 1);
+				udid = strdup(sudid.c_str());
+			}
+			system([[NSString stringWithFormat:@"plutil -replace bootstrap.child.DYLD_INSERT_LIBRARIES -string /opt/simject/simject.dylib %@/Library/Developer/CoreSimulator/Devices/%@/data/var/run/launchd_bootstrap.plist -s", NSHomeDirectory(), @(udid)] UTF8String]);
+			system("killall launchd_sim");
+		} else {
+			system([[NSString stringWithFormat:@"xcrun simctl spawn %s launchctl setenv DYLD_INSERT_LIBRARIES /opt/simject/simject.dylib", udid] UTF8String]);
+			system([[NSString stringWithFormat:@"xcrun simctl spawn %s launchctl setenv __XPC_DYLD_INSERT_LIBRARIES /opt/simject/simject.dylib", udid] UTF8String]);
+			system([[NSString stringWithFormat:@"xcrun simctl spawn %s launchctl stop com.apple.backboardd", udid] UTF8String]);
+		}
+		exit(EXIT_SUCCESS);
+	} else {
+		if (_exit) {
+			exit(EXIT_SUCCESS);
+		}
+	}
 }
 
 void injectUDIDs(const char *udid, BOOL all) {
-    string bootedDevices = exec("xcrun simctl list devices | grep -E Booted | sed \"s/^[ \\t]*//\"");
-    if (!bootedDevices.length()) {
-        printf("Error: No such booted devices\n");
-        exit(EXIT_FAILURE);
-    }
-    regex p("(.+) \\(([A-Z0-9\\-]+)\\) \\(Booted\\)");
-    smatch m;
-    BOOL foundAny = NO;
-    injectHeader();
-    while (regex_search(bootedDevices, m, p)) {
-        const char *bootedUDID = strdup(m[2].str().c_str());
-        if (all || (udid && !strcmp(bootedUDID, udid))) {
-            const char *bootedDevice = strdup(m[1].str().c_str());
-            inject(bootedUDID, bootedDevice, NO);
-            foundAny = YES;
-        }
-        bootedDevices = m.suffix().str();
-    }
-    if (!foundAny)
-        printf("Error: None of booted devices with UDID(s) specified is found\n");
-    exit(foundAny ? EXIT_SUCCESS : EXIT_FAILURE);
+	string bootedDevices = exec("xcrun simctl list devices | grep -E Booted | sed \"s/^[ \\t]*//\"");
+	if (!bootedDevices.length()) {
+		printf("Error: No booted iOS Simulators were found.\n");
+		exit(EXIT_FAILURE);
+	}
+	regex p("(.+) \\(([A-Z0-9\\-]+)\\) \\(Booted\\)");
+	smatch m;
+	BOOL foundAny = NO;
+	injectHeader();
+	while (regex_search(bootedDevices, m, p)) {
+		const char *bootedUDID = strdup(m[2].str().c_str());
+		if (all || (udid && !strcmp(bootedUDID, udid))) {
+			const char *bootedDevice = strdup(m[1].str().c_str());
+			inject(bootedUDID, bootedDevice, NO);
+			foundAny = YES;
+		}
+		bootedDevices = m.suffix().str();
+	}
+	if (!foundAny) {
+		printf("Error: No booted iOS Simulators with an UUID matching the specified UUID was found.\n");
+	}
+	exit(foundAny ? EXIT_SUCCESS : EXIT_FAILURE);
 }
 
 NSString *XcodePath() {
-    char buffer[128];
-    size_t len = readlink("/var/db/xcode_select_link", buffer, 128);
-    return len ? [NSString stringWithUTF8String:buffer] : nil;
+	char buffer[128];
+	size_t len = readlink("/var/db/xcode_select_link", buffer, 128);
+	return len ? [NSString stringWithUTF8String:buffer] : nil;
 }
 
 double XcodeVersion() {
 	NSBundle *simulatorBundle = [NSBundle bundleWithPath:[XcodePath() stringByAppendingPathComponent:@"/Applications/Simulator.app/"]];
-    if (simulatorBundle == nil)
-    	simulatorBundle = [NSBundle bundleWithPath:[XcodePath() stringByAppendingPathComponent:@"/Applications/iOS Simulator.app/"]];
-    return [[simulatorBundle objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey] doubleValue];
+	if (simulatorBundle == nil)
+		simulatorBundle = [NSBundle bundleWithPath:[XcodePath() stringByAppendingPathComponent:@"/Applications/iOS Simulator.app/"]];
+	return [[simulatorBundle objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey] doubleValue];
 }
 
 int main(int argc, char *const argv[]) {
 	double xcodeVersion = 0;
-	iOS7 = (xcodeVersion = XcodeVersion()) < 600.0; // Roughly Xcode 6.2 (and only on Mavericks)
-    if (argc == 2) {
-        if (!strcmp(argv[1], "all")) {
-            injectUDIDs(NULL, YES);
-        } else if (!strcmp(argv[1], "help")) {
-            printUsage();
-            exit(EXIT_SUCCESS);
-        }
-    }
-    int opt;
-    char *device = NULL, *version = NULL, *udid = NULL;
-    int deviceFlag = 0, versionFlag = 0, udidFlag = 0;
-    while ((opt = getopt(argc, argv, "d:v:i:l")) != -1) {
-        switch (opt) {
-            case 'd':
-                device = strdup(optarg);
-                if (*device == '-') {
-                    device = NULL;
-                    printf("Error: Device is entered incorrectly\n");
-                }
-                deviceFlag = 1;
-                break;
-            case 'v': {
-                if (!regex_match(version = strdup(optarg), regex("\\d+\\.\\d+"))) {
-                    version = NULL;
-                    printf("Error: Version is entered incorrectly\n");
-                }
-                versionFlag = 1;
-                break;
-            }
-            case 'i':
-                if (!regex_match(udid = strdup(optarg), regex("[A-Z0-9\\-]+"))) {
-                    udid = NULL;
-                    printf("Error: UDID is entered incorrectly\n");
-                }
-                udidFlag = 1;
-                break;
-            case 'l':
-            	iOS7 = YES;
-            	break;
-            default:
-                printUsage();
-                exit(EXIT_FAILURE);
-        }
-    }
-    if (udidFlag || deviceFlag || versionFlag || iOS7) {
-        if (xcodeVersion < 800.0) {
-            printf("Warning: The selected Xcode version does not support multiple simulators, booting this device could cause the old one to stop (if not the same)\n");
-        }
-        if (!(udidFlag != (deviceFlag && versionFlag)) && !iOS7) {
-            printUsage();
-            exit(EXIT_FAILURE);
-        }
-    }
-    if (!udidFlag && !deviceFlag && !versionFlag) {
-        injectHeader();
-        inject("booted", NULL, YES);
-    }
-    if (udidFlag) {
-        injectUDIDs(udid, NO);
-    } else {
-        NSString *devicesString = [NSString stringWithUTF8String:exec("xcrun simctl list devices -j").c_str()];
-        NSError *error = nil;
-        NSDictionary *devices = [NSJSONSerialization JSONObjectWithData:[devicesString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&error][@"devices"];
-        if (error || devices == nil) {
-            printf("Error: Could not list available devices\n");
-            exit(EXIT_FAILURE);
-        }
-        NSArray *runtime = devices[[NSString stringWithFormat:@"iOS %s", version]];
-        if (runtime == nil || runtime.count == 0) {
-            printf("Error: iOS %s runtime is not installed, or not supported\n", version);
-            exit(EXIT_FAILURE);
-        }
-        for (NSDictionary *entry in runtime) {
-            const char *state = [entry[@"state"] UTF8String];
-            const char *name = [entry[@"name"] UTF8String];
-            const char *udid = [entry[@"udid"] UTF8String];
-            if (!strcmp(name, device)) {
-                if (strcmp(state, "Booted")) {
-                    printf("Error: This device (%s, %s) is not yet booted up\n", name, udid);
-                    exit(EXIT_FAILURE);
-                }
-                injectHeader();
-                inject(udid, name, YES);
-            }
-        }
-    }
-    printf("Error: Could not find any booted device with matching information\n");
-    exit(EXIT_FAILURE);
+	iOS7 = (xcodeVersion = XcodeVersion()) < 600.0; // Should be around where Xcode 6.2 is (and only on Mavericks)
+	if (argc == 2) {
+		if (!strcmp(argv[1], "all")) {
+			injectUDIDs(NULL, YES);
+		} else if (!strcmp(argv[1], "help")) {
+			printUsage();
+			exit(EXIT_SUCCESS);
+		}
+	}
+	int opt;
+	char *device = NULL, *version = NULL, *udid = NULL;
+	int deviceFlag = 0, versionFlag = 0, udidFlag = 0;
+	while ((opt = getopt(argc, argv, "d:v:i:l:h")) != -1) {
+		switch (opt) {
+			case 'd':
+				device = strdup(optarg);
+				if (*device == '-') {
+					device = NULL;
+					printf("ERROR: iOS Simulator device parameter was entered incorrectly.\n");
+				}
+				deviceFlag = 1;
+				break;
+			case 'v': {
+				if (!regex_match(version = strdup(optarg), regex("\\d+\\.\\d+"))) {
+					version = NULL;
+					printf("ERROR: iOS version was entered incorrectly.\n");
+				}
+				versionFlag = 1;
+				break;
+			}
+			case 'i':
+				if (!regex_match(udid = strdup(optarg), regex("[A-Z0-9\\-]+"))) {
+					udid = NULL;
+					printf("ERROR: UUID was entered incorrectly.\n");
+				}
+				udidFlag = 1;
+				break;
+			case 'l':
+				iOS7 = YES;
+				break;
+			case 'h':
+				printUsage();
+				exit(EXIT_SUCCESS);
+			default:
+				printUsage();
+				exit(EXIT_FAILURE);
+		}
+	}
+	if (udidFlag || deviceFlag || versionFlag || iOS7) {
+		if (xcodeVersion < 800.0) {
+			printf("WARNING: The selected Xcode version does not support multiple running iOS Simulator instances simultaneously. Booting this device may cause any other existing instances of the iOS Simulator to terminate.\n");
+		}
+		if (!(udidFlag != (deviceFlag && versionFlag)) && !iOS7) {
+			printUsage();
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (!udidFlag && !deviceFlag && !versionFlag) {
+		injectHeader();
+		inject("booted", NULL, YES);
+	}
+	if (udidFlag) {
+		injectUDIDs(udid, NO);
+	} else {
+		NSString *devicesString = [NSString stringWithUTF8String:exec("xcrun simctl list devices -j").c_str()];
+		NSError *error = nil;
+		NSDictionary *devices = [NSJSONSerialization JSONObjectWithData:[devicesString dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&error][@"devices"];
+		if (error || devices == nil) {
+			printf("ERROR: Could not list available iOS Simulator devices\n");
+			exit(EXIT_FAILURE);
+		}
+		NSArray *runtime = devices[[NSString stringWithFormat:@"iOS %s", version]];
+		if (runtime == nil || runtime.count == 0) {
+			printf("ERROR: iOS %s runtime is not installed, or not supported by simject\n", version);
+			exit(EXIT_FAILURE);
+		}
+		for (NSDictionary *entry in runtime) {
+			const char *state = [entry[@"state"] UTF8String];
+			const char *name = [entry[@"name"] UTF8String];
+			const char *udid = [entry[@"udid"] UTF8String];
+			if (!strcmp(name, device)) {
+				if (strcmp(state, "Booted")) {
+					printf("ERROR: This device (%s, %s) has not yet booted up.\n", name, udid);
+					exit(EXIT_FAILURE);
+				}
+				injectHeader();
+				inject(udid, name, YES);
+			}
+		}
+	}
+	printf("ERROR: Could not find any booted iOS Simulator devices with the specified parameters.\n");
+	exit(EXIT_FAILURE);
 }
--- simject/simject.xm
+++ simject/simject.xm
@@ -1,99 +1,100 @@
 #import "../simject.h"
 #import <dlfcn.h>
 
-// Due to the fact that many developers use FLEX (or its variants) to inspect iOS simulator:
-// There exist processes that could crash when FLEX is injected and that could decrease performance of simulator alot
-// Those processes load UIKit, but they are not actually GUI apps so there's no point loading this kind of tweak
+// Since many iOS tweak developers use FLEXible (or some variant of that tweak) to inspect the iOS Simulator...
+// There are some processes that can crash when FLEXible is injected into them, significantly decreasing overall performance.
+// These processes do indeed load UIKit as a library, but they do not actually present a GUI so there is no point in injecting FLEXible into them.
 static NSArray *blackListForFLEX = @[@"com.apple.Search.framework", @"com.apple.accessibility.AccessibilityUIServer"];
 
 NSArray *simjectGenerateDylibList() {
-    NSString *processName = [[NSProcessInfo processInfo] processName];
-    // launchctl, you are a special case
-    if ([processName isEqualToString:@"launchctl"]) {
-        return nil;
-    }
-    // Create an array containing all the filenames in dylibDir (/opt/simject)
-    NSError *e = nil;
-    NSArray *dylibDirContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:dylibDir error:&e];
-    if (e) {
-        return nil;
-    }
-    // Read current bundle identifier
-    NSString *bundleIdentifier = NSBundle.mainBundle.bundleIdentifier;
-    // We're only interested in the plist files
-    NSArray *plists = [dylibDirContents filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF ENDSWITH %@", @"plist"]];
-    // Create an empty mutable array that will contain a list of dylib paths to be injected into the target process
-    NSMutableArray *dylibsToInject = [NSMutableArray array];
-    // Loop through the list of plists
-    for (NSString *plist in plists) {
-        // Don't inject simject itself
-        if ([plist isEqualToString:@"simject.plist"]) {
-            continue;
-        }
-        // Check if a dylib is FLEX and the current process is in blacklist
-        if ([[plist uppercaseString] rangeOfString:@"FLEX" options:NSLiteralSearch].location != NSNotFound && [blackListForFLEX containsObject:bundleIdentifier]) {
-            continue;
-        }
-        // We'll want to deal with absolute paths, so append the filename to dylibDir
-        NSString *plistPath = [dylibDir stringByAppendingPathComponent:plist];
-        NSDictionary *filter = [NSDictionary dictionaryWithContentsOfFile:plistPath];
-        // A boolean that indicates if the dylib is already injected
-        BOOL isInjected = NO;
-        // If supported iOS versions are specified, we check it first
-        NSArray *supportedVersions = filter[@"CoreFoundationVersion"];
-        if (supportedVersions) {
-            if (supportedVersions.count != 1 && supportedVersions.count != 2)
-                continue; // Supported versions are in wrong format, we should skip
-            if (supportedVersions.count == 1 && [supportedVersions[0] doubleValue] > kCFCoreFoundationVersionNumber) {
-                continue; // Doesn't meet lower bound
-            }
-            if (supportedVersions.count == 2 && ([supportedVersions[0] doubleValue] > kCFCoreFoundationVersionNumber || [supportedVersions[1] doubleValue] < kCFCoreFoundationVersionNumber)) {
-                continue; // Outside bounds
-            }
-        }
-        // Decide whether to load the dylib from bundles
-        for (NSString *entry in filter[@"Filter"][@"Bundles"]) {
-            // Now check if this bundle is loaded in this application or not
-            if (!CFBundleGetBundleWithIdentifier((CFStringRef)entry)) {
-                // Skip because this application doesn't load it
-                continue;
-            }
-            [dylibsToInject addObject:[[plistPath stringByDeletingPathExtension] stringByAppendingString:@".dylib"]];
-            isInjected = YES;
-            break;
-        }
-        if (!isInjected) {
-            // Decide whether to load the dylib from executables
-            for (NSString *process in filter[@"Filter"][@"Executables"]) {
-                if ([process isEqualToString:processName]) {
-                    [dylibsToInject addObject:[[plistPath stringByDeletingPathExtension] stringByAppendingString:@".dylib"]];
-                    isInjected = YES;
-                    break;
-                }
-            }
-        }
-        if (!isInjected) {
-            // Decide whether to load the dylib from classes
-            for (NSString *clazz in filter[@"Filter"][@"Classes"]) {
-                // Also check if this class is loaded in this application or not
-                if (!NSClassFromString(clazz)) {
-                    // This class couldn't be loaded, skip
-                    continue;
-                }
-                // It's ok to add this dylib at this point
-                [dylibsToInject addObject:[[plistPath stringByDeletingPathExtension] stringByAppendingString:@".dylib"]];
-                isInjected = YES;
-                break;
-            }
-        }
-    }
-    return dylibsToInject;
+	NSString *processName = [[NSProcessInfo processInfo] processName];
+	// launchctl, you are a special case
+	if ([processName isEqualToString:@"launchctl"]) {
+		return nil;
+	}
+	// Create an array containing all the filenames in dylibDir (/opt/simject)
+	NSError *e = nil;
+	NSArray *dylibDirContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:dylibDir error:&e];
+	if (e) {
+		return nil;
+	}
+	// Read current bundle identifier
+	NSString *bundleIdentifier = NSBundle.mainBundle.bundleIdentifier;
+	// We're only interested in the plist files
+	NSArray *plists = [dylibDirContents filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"SELF ENDSWITH %@", @"plist"]];
+	// Create an empty mutable array that will contain a list of dylib paths to be injected into the target process
+	NSMutableArray *dylibsToInject = [NSMutableArray array];
+	// Loop through the list of plists
+	for (NSString *plist in plists) {
+		// Don't inject simject itself
+		if ([plist isEqualToString:@"simject.plist"]) {
+			continue;
+		}
+		// Check if the current dylib is FLEXible and if the current process is in the blacklist
+		if ([[plist uppercaseString] rangeOfString:@"FLEX" options:NSLiteralSearch].location != NSNotFound && [blackListForFLEX containsObject:bundleIdentifier]) {
+			continue;
+		}
+		// We'll want to deal with absolute paths, so append the filename to dylibDir
+		NSString *plistPath = [dylibDir stringByAppendingPathComponent:plist];
+		NSDictionary *filter = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+		// This boolean indicates whether or not the dylib has already been injected
+		BOOL isInjected = NO;
+		// If supported iOS versions are specified within the plist, we check those first
+		NSArray *supportedVersions = filter[@"CoreFoundationVersion"];
+		if (supportedVersions) {
+			if (supportedVersions.count != 1 && supportedVersions.count != 2) {
+				continue; // Supported versions are in the wrong format, we should skip
+			}
+			if (supportedVersions.count == 1 && [supportedVersions[0] doubleValue] > kCFCoreFoundationVersionNumber) {
+				continue; // Doesn't meet lower bound
+			}
+			if (supportedVersions.count == 2 && ([supportedVersions[0] doubleValue] > kCFCoreFoundationVersionNumber || [supportedVersions[1] doubleValue] < kCFCoreFoundationVersionNumber)) {
+				continue; // Outside bounds
+			}
+		}
+		// Decide whether or not to load the dylib based on the Bundles values
+		for (NSString *entry in filter[@"Filter"][@"Bundles"]) {
+			// Check to see whether or not this bundle is actually loaded in this application or not
+			if (!CFBundleGetBundleWithIdentifier((CFStringRef)entry)) {
+				// If not, skip it
+				continue;
+			}
+			[dylibsToInject addObject:[[plistPath stringByDeletingPathExtension] stringByAppendingString:@".dylib"]];
+			isInjected = YES;
+			break;
+		}
+		if (!isInjected) {
+			// Decide whether or not to load the dylib based on the Executables values
+			for (NSString *process in filter[@"Filter"][@"Executables"]) {
+				if ([process isEqualToString:processName]) {
+					[dylibsToInject addObject:[[plistPath stringByDeletingPathExtension] stringByAppendingString:@".dylib"]];
+					isInjected = YES;
+					break;
+				}
+			}
+		}
+		if (!isInjected) {
+			// Decide whether or not to load the dylib based on the Classes values
+			for (NSString *clazz in filter[@"Filter"][@"Classes"]) {
+				// Also check if this class is loaded in this application or not
+				if (!NSClassFromString(clazz)) {
+					// This class couldn't be loaded, skip
+					continue;
+				}
+				// It's fine to add this dylib at this point
+				[dylibsToInject addObject:[[plistPath stringByDeletingPathExtension] stringByAppendingString:@".dylib"]];
+				isInjected = YES;
+				break;
+			}
+		}
+	}
+	return dylibsToInject;
 }
 
 %ctor {
-    // Inject any dylib meant to be run for this application
-    for (NSString *dylib in simjectGenerateDylibList()) {
-        HBLogDebug(@"Injecting %@ into %@", dylib, NSBundle.mainBundle.bundleIdentifier);
-        dlopen([dylib UTF8String], RTLD_LAZY | RTLD_GLOBAL);
-    }
+	// Inject any dylib meant to be run for this application
+	for (NSString *dylib in simjectGenerateDylibList()) {
+		HBLogDebug(@"Injecting %@ into %@", dylib, NSBundle.mainBundle.bundleIdentifier);
+		dlopen([dylib UTF8String], RTLD_LAZY | RTLD_GLOBAL);
+	}
 }
